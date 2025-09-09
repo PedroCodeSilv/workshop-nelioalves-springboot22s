d36e3450cfb241e8230e446c4d49e6c6601b0fb1
Pedro Cordeiro da Silva <p.co.silv11@gmail.com>
2025-09-08 22:12:49 -0300

    Order, Instance, ISO 8601 - PART 2

--- src/main/java/com/springgo/course/config/TestConfig.java
+++ src/main/java/com/springgo/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.springgo.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.springgo.course.entities.Order;
 import com.springgo.course.entities.User;
+import com.springgo.course.repositories.OrderRepository;
 import com.springgo.course.repositories.UserRepository;
 
 @Configuration
@@ -18,11 +21,19 @@ public class TestConfig implements CommandLineRunner{
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private OrderRepository orderRepository;
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
+		
+		Order o1 = new Order(null, Instant.parse("2025-06-19T19:00:03Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2025-08-22T18:00:57Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2025-09-11T11:23:42Z"), u1);
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 	}	
 }
